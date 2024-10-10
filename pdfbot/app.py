674e83012d4f0fa76016bede7868aa09117bcee6
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2024-10-10 19:00:28 +0530

    Update app.py

--- pdfbot/app.py
+++ pdfbot/app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter  
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
